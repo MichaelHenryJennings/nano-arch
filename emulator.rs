9da831b652dc57ae6eaaeea1e84bf3ea9b1de39e
MichaelHenryJennings <michaeljennings@utexas.edu>
2026-01-05 19:08:35 -0600

    word-level memory representation in emulator

--- emulator.rs
+++ emulator.rs
@@ -6,20 +6,20 @@ fn slice(value : u32, low : u8, bits : u8) -> u32 {
     (value >> low) & ((1 << bits) - 1)
 }
 
-// TODO replace with image preprocessing?
-fn load(mem : &Vec<u8>, addr : u32) -> u32 {
-    mem[(4 * addr) as usize] as u32 + 
-    ((mem[(4 * addr + 1) as usize] as u32) << 8) + 
-    ((mem[(4 * addr + 2) as usize] as u32) << 16) + 
-    ((mem[(4 * addr + 3) as usize] as u32) << 24)
-}
+// // TODO replace with image preprocessing?
+// fn load(mem : &Vec<u8>, addr : u32) -> u32 {
+//     mem[(4 * addr) as usize] as u32 + 
+//     ((mem[(4 * addr + 1) as usize] as u32) << 8) + 
+//     ((mem[(4 * addr + 2) as usize] as u32) << 16) + 
+//     ((mem[(4 * addr + 3) as usize] as u32) << 24)
+// }
 
-fn store(mem : &mut Vec<u8>, addr : u32, data : u32) {
-    mem[(4 * addr) as usize] = data as u8;
-    mem[(4 * addr + 1) as usize] = (data >> 8) as u8;
-    mem[(4 * addr + 2) as usize] = (data >> 16) as u8;
-    mem[(4 * addr + 3) as usize] = (data >> 24) as u8;
-}
+// fn store(mem : &mut Vec<u8>, addr : u32, data : u32) {
+//     mem[(4 * addr) as usize] = data as u8;
+//     mem[(4 * addr + 1) as usize] = (data >> 8) as u8;
+//     mem[(4 * addr + 2) as usize] = (data >> 16) as u8;
+//     mem[(4 * addr + 3) as usize] = (data >> 24) as u8;
+// }
 
 // TODO replace with declarative rules-based system?
 fn main() -> Result<(), Error> {
@@ -30,29 +30,35 @@ fn main() -> Result<(), Error> {
     }
 
     // initialize processor state
-    let mut pc : u32 = 0;
+    let mut pc : usize = 0;
     let mut reg : Vec<u32> = vec![0; 32];
 
     // initialize memory from image file
     let image : Vec<u8> = fs::read(&args[1])?;
-    let mut mem : Vec<u8> = vec![0; 1 << 34];
+    if image.len() % 4 != 0 {
+        assert!(false); // TODO better error handling
+    }
+    let mut mem : Vec<u32> = vec![0; 1 << 32];
     let mut k : usize = 0;
-    while k < image.len() {
-        mem[k] = image[k];
+    while k < image.len() / 4 {
+        mem[k] = (image[4 * k] as u32) + 
+                 ((image[4 * k + 1] as u32) << 8) +
+                 ((image[4 * k + 2] as u32) << 16) +
+                 ((image[4 * k + 3] as u32) << 24);
         k += 1;
     }
     // println!("Beginning emulation.");
 
     // fetch-decode-execute loop
     loop {
-        let instruction : u32 = load(&mem, pc);
+        let instruction : u32 = mem[pc];
         if slice(instruction, 10, 22) == 0b0000000000000000000000 { // halt
             let mut j : u32 = 0;
             let p : usize = slice(instruction, 5, 5) as usize;
             let l : usize = slice(instruction, 0, 5) as usize;
             // println!("{}", if reg[l] == 0 {"No results to print."} else {"Emulation complete, printing results:"});
             while j < reg[l] {
-                println!("{}", load(&mem, reg[p] + j));
+                println!("{}", mem[(reg[p] + j) as usize]);
                 j += 1;
             }
             break;
@@ -60,17 +66,17 @@ fn main() -> Result<(), Error> {
             let c : usize = slice(instruction, 5, 5) as usize;
             if reg[c] != 0 {
                 let t : usize = slice(instruction, 0, 5) as usize;
-                pc = reg[t];
+                pc = reg[t] as usize;
                 continue; // avoid incrementing pc as normal
             }
         } else if slice(instruction, 10, 22) == 0b0100000000000000000000 { // ld
             let p : usize = slice(instruction, 5, 5) as usize;
             let t : usize = slice(instruction, 0, 5) as usize;
-            reg[t] = load(&mem, reg[p]);
+            reg[t] = mem[reg[p] as usize];
         } else if slice(instruction, 10, 22) == 0b0101000000000000000000 { // st
             let p : usize = slice(instruction, 5, 5) as usize;
             let t : usize = slice(instruction, 0, 5) as usize;
-            store(&mut mem, reg[p], reg[t]);
+            mem[reg[p] as usize] = reg[t];
         } else if slice(instruction, 22, 10) == 0b0110000000 { // mov
             let w : u8 = slice(instruction, 21, 1) as u8;
             let i : u32 = slice(instruction, 0, 16) as u32;
